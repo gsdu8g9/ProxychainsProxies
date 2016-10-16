fa46a9a4bf6e44ea1d9e240d8803efcaa7afa367
Spaddex <Spaddex@swehack.org>
2016-10-16 19:37:35 +0200

    More sources + socksVersion
    Added more sources to gather proxies from, plus added code to determine socksversion.

--- ProxychainsProxies.py
+++ ProxychainsProxies.py
@@ -7,6 +7,8 @@ import re
 import socksChecker #from https://github.com/BeastsMC/SOCKS-Proxy-Checker
 import os
 import subprocess
+import socket
+from struct import *
 
 userAgent={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"}
 foundIP=[]
@@ -24,7 +26,6 @@ def fromGatherProxies():
 			foundIP.append(ip[0])
 		if len(port)!=0:
 			foundPort.append(port[0])
-
 
 def fromXroxy():
 	for page in range(0,5):
@@ -48,12 +49,23 @@ def fromXroxy():
 			if len(port) != 0:
 				foundPort.append(port[3])
 
-
 def fromSamair():
-	for page in range(0,5):
-		req = requests.get("http://www.samair.ru/proxy/socks0"+str(page)+".htm", headers=userAgent)
-		raw_html = req.text
-		soup = BeautifulSoup(raw_html, "html.parser")
+	try:
+		for page in range(1,7):
+			req = requests.get("http://www.samair.ru/proxy/socks0"+str(page)+".htm", headers=userAgent)
+			raw_html = req.text
+			soup = BeautifulSoup(raw_html, "html.parser")
+			soup2 = soup.findAll("td")
+			for page in soup2:
+				ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(page))
+				port = re.findall(r'[0-9]{4,5}', str(page))
+				if len(ip) != 0:
+					foundIP.append(ip[0])
+				if len(port) != 0:
+					foundPort.append(port[0])
+
+	except Exception as e:
+		print "Could not connect to Samair: " + str(e)
 
 def printProxychainsConf():
 	template = open("template.conf", 'r')
@@ -66,15 +78,17 @@ def printProxychainsConf():
 				conf.write(each)
 			for singleproxy in proxies:
 				singleIP=singleproxy.split(":")
-				conf.write("socks4  " + str(singleIP[0]) + " " + str(singleIP[1]))
+				socksVerison=getSocksVersion(singleIP[0], int(singleIP[1].strip()))
+				if socksVerison!=0:
+					conf.write("socks"+str(socksVerison)+"  " + str(singleIP[0]) + " " + str(singleIP[1]))
 	except Exception as e:
 		print "something failed in the printProxychainsConf-function: " + str(e)
 
 def testProxies():
 	amount=len(foundIP)
 	with open("toTesting.txt", "a") as testingFactory:
+		print "Sending " + str(len(foundIP)) + " proxies to the doctor for diagnosis"
 		for x in range(0,amount):
-			print "To testing factory: " + str(foundIP[x])
 			testingFactory.write(str(foundIP[x]) + ":" + str(foundPort[x]) + "\n")
 	try:
 		subprocess.call("./bin/python ./socksChecker.py", cwd='./', shell=True)
@@ -82,8 +96,70 @@ def testProxies():
 		print "Could not validate proxies: " + str(e)
 
 
-def fromSpysRu():
-	pass
+
+####Code from socksChecker###
+def isSocks4(host, port, soc):
+	ipaddr = socket.inet_aton(host)
+	packet4 = "\x04\x01" + pack(">H", port) + ipaddr + "\x00"
+	soc.sendall(packet4)
+	data = soc.recv(8)
+	if (len(data) < 2):
+		# Null response
+		return False
+	if data[0] != "\x00":
+		# Bad data
+		return False
+	if data[1] != "\x5A":
+		# Server returned an error
+		return False
+	return True
+
+def isSocks5(host, port, soc):
+	soc.sendall("\x05\x01\x00")
+	data = soc.recv(2)
+	if (len(data) < 2):
+		# Null response
+		return False
+	if data[0] != "\x05":
+		# Not socks5
+		return False
+	if data[1] != "\x00":
+		# Requires authentication
+		return False
+	return True
+
+def getSocksVersion(proxy, port):
+	host = proxy.split(":")[0]
+	try:
+		port = int(proxy.split(":")[1])
+		if port < 0 or port > 65536:
+			print "Invalid: " + proxy
+			return 0
+	except:
+		print "Invalid: " + proxy
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(10)
+	try:
+		s.connect((host, port))
+		if (isSocks4(host, port, s)):
+			s.close()
+			return 4
+		elif (isSocks5(host, port, s)):
+			s.close()
+			return 5
+		else:
+			("Not a SOCKS: " + proxy)
+			s.close()
+			return 0
+	except socket.timeout:
+		print "Timeout: " + proxy
+		s.close()
+		return 0
+	except socket.error:
+		print "Connection refused: " + proxy
+		s.close()
+		return 0
+####
 
 def createFiles():
 	if os.path.exists("ok.txt"):
@@ -118,20 +194,18 @@ def main():
 	#createFiles()
 	fromGatherProxies()
 	fromXroxy()
-	#fromSamair()
+	fromSamair()
 	testProxies()
-
 	printProxychainsConf()
-	#emptyFiles()
+	emptyFiles()
 
 if __name__ == "__main__":
 	main()
 
 
 """
-Notes to self:
-Sen ska Samair fixas
-Se om det är en socks4 eller socks5 och lägga till i proxyconf-fien
-Ska göra så att funktionen createFiles fungerar
-Göra så subprocess ej behöver användas
+Todo:
+- Check if empty files are available, if not, create them for writing
+- Make the ip and port key-value pairs (So that if there are problems getting one port, it won't create a domino effect, making all the other proxies fail)
+
 """
